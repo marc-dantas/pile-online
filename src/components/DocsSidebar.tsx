dc6f1174b568ddb31324fa2db2633fe2f3c3f8a7
marc-dantas <marciodantasms@hotmail.com>
2024-11-09 16:30:56 -0300

    revised sidebar

--- src/components/DocsSidebar.tsx
+++ src/components/DocsSidebar.tsx
@@ -20,17 +20,21 @@ export default function DocsSidebar({ children }: DocsSidebarProps) {
                             <li><a href="/docs/howpileworks">How Pile works</a></li>
                             <li><a href="/docs/basics">Basics</a></li>
                         </SidebarGroup>
-                        <SidebarGroup title="Reference">
-                            <li><a href="/docs/introduction">Introduction</a></li>
+                        <SidebarGroup title="Operations">
                             <li><a href="/docs/math">Mathematical operations</a></li>
                             <li><a href="/docs/logic">Logical operations</a></li>
                             <li><a href="/docs/bitwise">Bitwise operations</a> </li>
                             <li><a href="/docs/stack">Stack manipulation operations</a></li>
+                        </SidebarGroup>
+                        <SidebarGroup title="Pile Structures">
                             <li><a href="/docs/controlflow">Control flow</a></li>
                             <li><a href="/docs/procs">Procedures</a></li>
                             <li><a href="/docs/defs">Definitions</a> </li>
+                        </SidebarGroup>
+                        <SidebarGroup title="Other">
                             <li><a href="/docs/builtins">Builtins</a></li>
                             <li><a href="/docs/io">I/O</a> </li>
+                            <li><a href="/docs/examples/">Code examples</a> </li>
                         </SidebarGroup>
                     </Sidebar>
                 </div>
