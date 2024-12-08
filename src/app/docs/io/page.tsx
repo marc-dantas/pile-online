2bbe24c235b24f2d8e3cc336123a4928ea7042d6
marc-dantas <marciodantasms@hotmail.com>
2024-12-08 20:46:01 -0300

    fixed title for I/O page

--- src/app/docs/io/page.tsx
+++ src/app/docs/io/page.tsx
@@ -1,10 +1,10 @@
 import DocsPage from "@/components/DocsPage";
 import { Title, Subtitle, Title2 } from "@/components/Docs";
-
+    
 export default function Docs() {
     return (
         <DocsPage>
-            <Title>Mathematical operations</Title>
+            <Title>I/O</Title>
             <Subtitle>This topic is meant to demonstrate I/O in Pile</Subtitle>
             <Title2>TODO</Title2>
         </DocsPage>
