0c3c3add4d1e4ad4531682220fdf0b596556d821
marc-dantas <marciodantasms@hotmail.com>
2024-12-08 23:26:05 -0300

    Better homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,27 +67,28 @@ export default function Home() {
                 <div className="hero-body">
                     <section id="learnmore" className="section">
                         <div className="container">
+                            <h2 className="title has-logo-font has-text-centered has-text-weight-bold">Learn More</h2>
                             <div className="columns is-multiline">
-                                <div className="column is-one-third">
-                                    <div className="box p-6">
-                                        <p className="title is-4 has-text-centered">Bridging the Gap</p>
-                                        <p className="is-size-5 has-text-justified has-text-left-mobile">
+                                <div className="column is-full">
+                                    <div className="tile is-child box p-6">
+                                        <p className="title is-5 has-text-centered">Bridging the Gap</p>
+                                        <p className="is-size-5">
                                             Pile is a comprehensive educational tool designed to bridge the gap between novice programmers and one of the most feared foundational computer science concepts.
                                         </p>
                                     </div>
                                 </div>
-                                <div className="column is-one-third">
-                                    <div className="box p-6">
-                                        <p className="title is-4 has-text-centered">Understanding Stack-Based Programming</p>
-                                        <p className="is-size-5 has-text-justified has-text-left-mobile">
+                                <div className="column is-full">
+                                    <div className="tile is-child box p-6">
+                                        <p className="title is-5 has-text-centered">Understanding Stack-Based Programming</p>
+                                        <p className="is-size-5">
                                             With a focus on stack-based programming, Pile empowers learners to visualize how data is manipulated, fostering a deeper understanding of algorithms and data structures.
                                         </p>
                                     </div>
                                 </div>
-                                <div className="column is-one-third">
-                                    <div className="box p-6">
-                                        <p className="title is-4 has-text-centered">A Supportive Learning Environment</p>
-                                        <p className="is-size-5 has-text-justified has-text-left-mobile">
+                                <div className="column is-full">
+                                    <div className="tile is-child box p-6">
+                                        <p className="title is-5 has-text-centered">A Supportive Learning Environment</p>
+                                        <p className="is-size-5">
                                             Our goal is to create a supportive environment where users can experiment, learn, and grow in their programming journey without the intimidation often associated with coding.
                                         </p>
                                     </div>
@@ -97,62 +98,52 @@ export default function Home() {
                     </section>
                     <section className="section">
                         <div className="container">
-                            <h2 className="title has-logo-font has-text-centered">Features</h2>
-                            <div className="box p-6">
-                                <div className="columns is-8 has-text-justified has-text-left-mobile">
-                                    <div className="column is-one-third">
-                                        <div className="content is-size-4">
-                                            <h4 className="has-text-weight-bold">Intuitive</h4>
-                                            <p>Designed for easier understanding and exponential learning curve for those who are familiar with other programming languages.</p>
-                                        </div>
-                                    </div>
-                                    <div className="column is-one-third">
-                                        <div className="content is-size-4">
-                                            <h4 className="has-text-weight-bold">Educational goals</h4>
-                                            <p>Pile is crafted as a &quot;teacher&quot; for the computer science students. Always aiming to be concise, promoting algorithmic and logical thinking.</p>
-                                        </div>
-                                    </div>
-                                    <div className="column is-one-third">
-                                        <div className="content is-size-4">
-                                            <h4 className="has-text-weight-bold">Focused on simplicity</h4>
-                                            <p>Pile&apos;s codebase, syntax, and runtime behavior always aims to be simple, practical and predictable.</p>
+                            <h2 className="title has-logo-font has-text-centered has-text-weight-bold">Features</h2>
+                            <div className="columns is-multiline">
+                                <div className="column is-one-third">
+                                    <div className="card">
+                                        <div className="card-content">
+                                            <p className="title is-4 has-logo-font">Intuitive</p>
+                                            <p>Pile is designed for easier understanding and an exponential learning curve for those familiar with other programming languages.</p>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="columns is-8 has-text-justified has-text-left-mobile">
-                                    <div className="column is-one-third">
-                                        <div className="content is-size-4">
-                                            <h4 className="has-text-weight-bold">Open Source</h4>
-                                            <p>All of the code and documentation is free and open source. Join a community-driven project that thrives on collaboration.</p>
-                                        </div>
-                                    </div>
-                                    <div className="column is-one-third">
-                                        <div className="content is-size-4">
-                                            <h4 className="has-text-weight-bold">Comprehensive Documentation</h4>
-                                            <p>Access thorough resources to guide your learning journey.</p>
-                                        </div>
-                                    </div>
-                                    <div className="column is-one-third">
-                                        <div className="content is-size-4">
-                                            <h4 className="has-text-weight-bold">And many more...</h4>
+                                <div className="column is-one-third">
+                                    <div className="card">
+                                        <div className="card-content">
+                                            <p className="title is-4 has-logo-font">Educational Goals</p>
+                                            <p>Pile is crafted as a "teacher" for computer science students, always aiming to be concise, promoting algorithmic and logical thinking.</p>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="section">
-                        <div className="container">
-                            <h2 className="title has-logo-font has-text-centered">Learn Pile</h2>
-                            <div className="box p-6">
-                                <div className="content">
-                                    <div className="columns is-multiline is-centered">
-                                        <div className="column is-full">
-                                            <p className="is-size-3">Dive into the fundamentals of Pile and discover the simplicity and power of stack-based programming!</p>
-                                            <p className="is-size-4">You can read the documentation of the Pile programming language to learn it.</p>
+                                <div className="column is-one-third">
+                                    <div className="card">
+                                        <div className="card-content">
+                                            <p className="title is-4 has-logo-font">Focused on Simplicity</p>
+                                            <p>Pile&apos;s codebase, syntax, and runtime behavior aim to be simple, practical, and predictable.</p>
                                         </div>
-                                        <div className="column is-narrow">
-                                            <a href="/docs" className="button is-rounded is-light">Go to Documentation</a>
+                                    </div>
+                                </div>
+                                <div className="column is-one-third">
+                                    <div className="card">
+                                        <div className="card-content">
+                                            <p className="title is-4 has-logo-font">Open Source</p>
+                                            <p>All of the code and documentation is free and open-source. Join a community-driven project that thrives on collaboration.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="column is-one-third">
+                                    <div className="card">
+                                        <div className="card-content">
+                                            <p className="title is-4 has-logo-font">Comprehensive Documentation</p>
+                                            <p>Pile offers a small and simple documentation for everyone. Learn with various examples, reference and theory. All included!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="column is-one-third">
+                                    <div className="card">
+                                        <div className="card-content">
+                                            <p className="title is-4 has-logo-font">And Many More...</p>
                                         </div>
                                     </div>
                                 </div>
@@ -161,20 +152,24 @@ export default function Home() {
                     </section>
                     <section className="section">
                         <div className="container">
-                            <h2 className="title has-logo-font has-text-centered">Help Build the Community</h2>
+                            <h2 className="title has-text-centered has-text-weight-bold has-logo-font">Learn Pile</h2>
+                            <p className="subtitle has-text-centered">Dive into the fundamentals of Pile and discover the simplicity and power of stack-based programming!</p>
                             <div className="box p-6">
-                                <div className="content is-size-4">
-                                    <div className="columns is-multiline is-centered">
-                                        <div className="column is-full">
-                                            <div className="content">
-                                                <p className="is-size-3">Pile is just getting started, and we invite you to be part of its journey!</p>
-                                                <p className="is-size-4">Join us in shaping the future of this unique language by collaborating on the project and helping grow the Pile community together.</p>
-                                                <p className="is-size-4">Connect with fellow enthusiasts and developers on our official Pile Discord server—your insights and ideas are essential to building something extraordinary!</p>
-                                            </div>
-                                        </div>
-                                        <div className="column is-narrow">
-                                            <a href="https://github.com/marc-dantas/pile" className="button is-link is-rounded">Discord server</a>
-                                        </div>
+                                <div className="columns is-multiline is-centered">
+                                    <div className="column content is-full">
+                                        <h4 className="title is-4">Why learn Pile?</h4>
+                                        <p className="is-size-4">Whether you're a beginner or an experienced developer looking to explore a fresh approach to coding, Pile can still be an amazing tool for teachers, students and even mathematicians.</p>
+                                        <p className="is-size-4">With a comprehensive and small documentation, you can learn Pile in a few hours and start learning even more!</p>
+                                        <h4 className="title is-4">What is in the documentation?</h4>
+                                        <ul className="is-size-4">
+                                            <li>Brief introduction and explanation of Pile and how it works</li>
+                                            <li>Language Reference</li>
+                                            <li>Code examples</li>
+                                            <li>And more!</li>
+                                        </ul>
+                                    </div>
+                                    <div className="column is-narrow">
+                                        <a href="/docs" className="button is-rounded is-light">Go to Documentation</a>
                                     </div>
                                 </div>
                             </div>
@@ -182,21 +177,23 @@ export default function Home() {
                     </section>
                     <section className="section">
                         <div className="container">
-                            <h2 className="title has-logo-font has-text-centered">Contributing</h2>
+                            <h2 className="title has-logo-font has-text-centered has-text-weight-bold">Contributing</h2>
+                            <p className="subtitle has-text-centered">Become an active part of Pile&apos;s evolution!</p>
                             <div className="box p-6">
                                 <div className="columns is-multiline is-centered">
                                     <div className="column is-full">
-                                        <div className="content">
-                                            <p className="is-size-3">
-                                                Become an active part of Pile&apos;s evolution!
-                                            </p>
-                                            <p className="is-size-4">
-                                                Visit the official GitHub repository to explore in-depth examples, access the full source code, and contribute directly to this innovative, stack-based language. Whether you&apos;re refining existing features, adding new ones, or simply learning, your contributions are invaluable to shaping Pile&apos;s future.
-                                            </p>
-                                        </div>
+                                        <h4 className="title is-4">Contributing to Pile&apos;s implementation</h4>
+                                        <p className="is-size-4">Visit the official GitHub repository to explore in-depth examples, access the full source code, and contribute directly to this innovative, stack-based language. Whether you're refining existing features, adding new ones, or simply learning, your contributions are invaluable to shaping Pile&apos;s future.</p>
                                     </div>
                                     <div className="column is-narrow">
-                                        <a href="https://github.com/marc-dantas/pile" className="button is-dark is-rounded">GitHub Repository</a>
+                                        <a href="https://github.com/marc-dantas/pile" className="button is-dark is-rounded">Implementation Repository</a>
+                                    </div>
+                                    <div className="column content is-full">
+                                        <h4 className="title is-4">Contributing to Pile&apos;s website</h4>
+                                        <p className="is-size-4">Contribute to this website. If you have any suggestions, Pull Requests are welcome!</p>
+                                    </div>
+                                    <div className="column is-narrow">
+                                        <a href="https://github.com/marc-dantas/pile-online" className="button is-dark is-rounded">Website Repository</a>
                                     </div>
                                 </div>
                             </div>
