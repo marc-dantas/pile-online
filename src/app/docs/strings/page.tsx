2bb3a389eadffcbe1aa5584f5a599b8c9f61d7b3
marc-dantas <marciodantasms@hotmail.com>
2025-03-26 20:08:18 -0300

    happy eslint by the way

--- src/app/docs/strings/page.tsx
+++ src/app/docs/strings/page.tsx
@@ -2,7 +2,7 @@ import DocsPage from "@/components/DocsPage";
 import { Title, Subtitle, Title2, P } from "@/components/Docs";
 import Code, { S } from "@/components/Code";
 // Code highlighting components
-import { Kw, N, Op, C } from "@/components/Code";
+import { C } from "@/components/Code";
 
 export default function Docs() {
     return (
@@ -22,7 +22,7 @@ export default function Docs() {
             <P>You can use <a href="/docs/sequences">sequence operations</a> to manipulate strings in Pile.</P>
 
             <Title2>Character Literals</Title2>
-            <P>Sometimes you don't need Pile's interpreter to allocate just a single character in memory just to be able to use it as text. You can avoid that by using character literals</P>
+            <P>Sometimes you don&apos;t need Pile&apos;s interpreter to allocate just a single character in memory just to be able to use it as text. You can avoid that by using character literals</P>
             <P>To push a character on the stack, you use single quotes (<code>&apos;</code>). You do not need to close it with another <code>&apos;</code>.</P>
             <Code>
                 <C x="# This is not a typo, you actually don't need to close it" /><br />
