fb2b4e067f356937ebbff6d7f59771d55f9ae40e
marc-dantas <marciodantasms@hotmail.com>
2025-10-14 20:37:43 -0300

    new builtins

--- src/app/docs/builtins/page.tsx
+++ src/app/docs/builtins/page.tsx
@@ -6,51 +6,51 @@ export default function Docs() {
         <DocsPage>
             <Title>Builtins</Title>
             <Subtitle>This topic is meant to document builtins in Pile</Subtitle>
-            <Title2>What is a builtin?</Title2>
-            <P>A builtin is an operation that is built into Pile&apos;s interpreter. Builtins can have various use cases and help the developer to create programs with basic features.</P>
+            <Title2>Overview</Title2>
+            <P>A builtin is an operation that is built into Pile&apos;s interpreter. Generally they are more complex compared to the standard operations (like <code>+</code>, <code>-</code>, <code>?</code>). Builtins can have various use cases and help the developer to create programs with basic features.</P>
             <Title3>I/O</Title3>
             <Table>
                 <thead className="has-text-weight-bold">
                     <tr>
                         <td>Builtin</td>
-                        <td>Description</td>
+                        <td colSpan={2}>Description</td>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td><code>print</code></td>
+                        <td><code>open</code></td>
                         <td>
-                            Prints the last element on the stack to stdout.
+                            <span className="notation">( path -- file )</span>
+                        </td>
+                        <td>
+                            Pops a <code>path</code> as a string and opens a the file located in it and pushes the corresponding <code>file</code> object.
                         </td>
                     </tr>
                     <tr>
-                        <td><code>println</code></td>
+                        <td><code>read</code></td>
                         <td>
-                            Prints the last element on the stack to stdout with a newline character at the end.
+                            <span className="notation">( file -- xs )</span>
+                        </td>
+                        <td>
+                            Pops a <code>file</code> object on the stack and reads until EOF and pushes the read content as a string.
                         </td>
                     </tr>
                     <tr>
-                        <td><code>eprint</code></td>
+                        <td><code>readline</code></td>
                         <td>
-                            Prints the last element on the stack to stderr.
+                            <span className="notation">( file -- line )</span>
+                        </td>
+                        <td>
+                            Pops a <code>file</code> object on the stack and reads until the end of the line and pushes the read line as a string.
                         </td>
                     </tr>
                     <tr>
-                        <td><code>eprintln</code></td>
+                        <td><code>write</code></td>
                         <td>
-                            Prints the last element on the stack to stderr with a newline character at the end.
+                            <span className="notation">( file xs -- )</span>
                         </td>
-                    </tr>
-                    <tr>
-                        <td><code>input</code></td>
                         <td>
-                            Reads input from stdin until EOF and pushes the input on top of the stack.
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><code>inputln</code></td>
-                        <td>
-                            Reads only a line from stdin and pushes the input on top of the stack.
+                            Pops a <code>file</code> object on the stack and writes the specified content <code>xs</code> as a string.
                         </td>
                     </tr>
                 </tbody>
